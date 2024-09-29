080a9b3bb06b61aff9c499f61b67faf94aa1401d
Hugo Sauvage <hugosauvage@MacBook-Air-de-Hugo.local>
2024-09-29 17:19:26 +0200

    caroussel ajouté

---
+++ assets/js/swiper.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        loop: true
+    });
+});
